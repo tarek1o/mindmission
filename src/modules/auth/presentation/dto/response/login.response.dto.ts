49932fc6f0750fcbe967d8eaa98285be220c0f9e
Tarek Eslam <89143141+tarek1o@users.noreply.github.com>
2026-01-07 00:05:26 +0200

    fix: correct import path for LoginViewModel in login response DTO

--- src/modules/auth/presentation/dto/response/login.response.dto.ts
+++ src/modules/auth/presentation/dto/response/login.response.dto.ts
@@ -1,4 +1,4 @@
-import { LoginViewModel } from 'src/modules/auth/application/view-models/login..view-model';
+import { LoginViewModel } from 'src/modules/auth/application/view-models/login.view-model';
 import { UserDetailsResponseDto } from 'src/modules/user/presentation/dto/response/user-details.response.dto';
 
 export class LoginResponseDto {
